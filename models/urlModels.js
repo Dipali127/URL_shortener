c615881207b36e91273c59fa1ae08e5faf079600
Dipali <dipalibohara500@gmail.com>
2024-03-21 00:59:49 -0700

    added URL model

---
+++ models/urlModels.js
@@ -0,0 +1,26 @@
+// longUrl: Represents the original, long URL. It's of type String and is required.
+// shortCode: Represents the unique code generated for the short URL. It's of type String, required, and has the unique constraint, ensuring each short URL has a unique code.
+// clickCount: Represents the number of times the short URL has been clicked. It's of type Number and has a default value of 0.
+// {timestamps:true}: This option automatically adds createdAt and updatedAt fields to the document, which can be useful for tracking when a URL was created or last updated.
+
+
+
+const mongoose = require('mongoose');
+const urlSchema = new mongoose.Schema({
+    longUrl:{
+        type:String,
+        required:true
+    },
+    shortCode:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    clickCount:{
+        type:Number,
+        default:0
+    },
+},{timestamps:true}) 
+
+module.exports = mongoose.model('URL',urlSchema);
+
