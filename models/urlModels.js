ea1e56eb6626685c22c67a034098e9a03f0e4b8e
Dipali <dipalibohara500@gmail.com>
2024-03-23 04:50:25 -0700

    update urlModel

--- models/urlModels.js
+++ models/urlModels.js
@@ -14,6 +14,10 @@ const urlSchema = new mongoose.Schema({
         required:true,
         unique:true
     },
+    urlClickcount:{
+        type:Number,
+        default:0
+    }
 },{timestamps:true}) 
 
 module.exports = mongoose.model('url',urlSchema);
