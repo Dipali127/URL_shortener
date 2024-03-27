77e23ece45fdd3db7b1984013113b2d5614fbd1d
Dipali <dipalibohara500@gmail.com>
2024-03-27 06:47:45 -0700

    Add descriptive comments to router

--- router/routes.js
+++ router/routes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const urlController = require('../controllers/urlController');
 
+// Route to generate a short URL for a long URL
 router.post('/generateShorturl', urlController.createShortURL);
+
+// Route to redirect the user to the long URL associated with the provided short code
 router.get('/:shortCode', urlController.redirectUrl);
+
+// Route to get the total number of clicks on a short URL
 router.get('/clickCount/:shortCode', urlController.clickTracker);
 
 module.exports = router;
